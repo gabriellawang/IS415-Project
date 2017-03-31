8ed374f42d0a59c0a0fd2cbc1e4d03c99d8d5412
JinYuan <jinyuan90@hotmail.com>
2017-03-31 22:01:17 +0800

    Modified UI with new input k, dynamic hours selectinput

--- server.r
+++ server.r
@@ -7,6 +7,7 @@ library(dplyr)
 library(rgdal)
 library(flows)
 library(data.table)
+library(stats)
 
 wd <- setwd(".")
 setwd(wd)
@@ -23,38 +24,56 @@ busstop_data <- read.csv(paste(project_path,sep = "/",paste(attribute_path,sep =
 busstop_data$BUS_STOP_N <- c(as.character(busstop_data$BUS_STOP_N))
 
 
-assign("data_file_name", NULL, envir = .GlobalEnv) 
+#----------------------------------------------------- Perform Once ----------------------------------------------------
+data_file_name <<- "2016-02-16.csv"
+
+ride_data <-  fread(paste(project_path,sep = "/",paste(attribute_path,sep = "/",data_file_name)))
+ride_data$RIDE_START_HOUR <- as.POSIXlt(ride_data$RIDE_START_TIME, format = "%H:%M:%S")$hour
+ride_data$BOARDING_STOP_STN <- c(as.character(ride_data$BOARDING_STOP_STN))
+ride_data$ALIGHTING_STOP_STN <- c(as.character(ride_data$ALIGHTING_STOP_STN))
+
+processed_data <- inner_join(ride_data,busstop_data,by=c("BOARDING_STOP_STN"="BUS_STOP_N"),suffix=c(".BOARDING",".ALIGHTING"))
+processed_data <- inner_join(processed_data,busstop_data,by=c("ALIGHTING_STOP_STN"="BUS_STOP_N"),suffix=c(".BOARDING",".ALIGHTING"))
+
+#set boarding x y as point
+coordinates(processed_data)<-~X.BOARDING+Y.BOARDING
+processed_data$BOARDING_AREA <- over(processed_data,plan_area_sdf)$PLN_AREA_N
+
+
+#convert sub_data as df again.
+processed_data <- as.data.frame(processed_data)
+
+#set alighting x y as point
+coordinates(processed_data)<-~X.ALIGHTING+Y.ALIGHTING
+processed_data$ALIGHTING_AREA <- over(processed_data,plan_area_sdf)$PLN_AREA_N
+
+original_data <<- processed_data
+
+first_load <<- TRUE
+#----------------------------------------------------- Perform Once End ----------------------------------------------------
+
+
+
+
+
 
 options(shiny.maxRequestSize=600*1024^2)
 shinyServer(function(input, output, session) {
   
   get_data <- reactive(function(inFile){
-    
-    
-    data_file_name <- get('data_file_name', envir=.GlobalEnv) 
-    
-    if(is.null(inFile)){
-      if (is.null(data_file_name)){
-        data_original <<-  fread(paste(project_path,sep = "/",paste(attribute_path,sep = "/","2016-02-16.csv")))
-        data_original <<- process_ride_data()(data_original)
-        assign("data_file_name", "2016-02-16", envir = .GlobalEnv) 
+    if (!is.null(inFile)){
+      if (inFile$name != data_file_name){
+        data <- fread(inFile$datapath, header = T, sep = ",")
+        cat("file name =",inFile$name)
+        data_file_name <<- inFile$name
+        original_data <<- process_ride_data()(data)
       }
-      processed_ride_data <- data_original
-      
-    }else{
-      if (data_file_name != inFile$name){
-        #processed_ride_data <- data_original
-        data_original  <<- fread(inFile$datapath, header = T, sep = ",")
-        data_original <<- process_ride_data()(data_original)
-        assign("data_file_name", inFile$name, envir = .GlobalEnv) 
-      }
-      processed_ride_data <- data_original
-    }  
-    processed_ride_data
+    }
+    return (original_data)
   })
   
   process_ride_data <- reactive(function(data_original){
-    cat("process_ride_data() executed: ")
+    cat("process_ride_data() executed ")
     ride_data <- data_original
     ride_data$RIDE_START_HOUR <- as.POSIXlt(ride_data$RIDE_START_TIME, format = "%H:%M:%S")$hour
     ride_data$BOARDING_STOP_STN <- c(as.character(ride_data$BOARDING_STOP_STN))
@@ -75,13 +94,10 @@ shinyServer(function(input, output, session) {
     processed_data$ALIGHTING_AREA <- over(processed_data,plan_area_sdf)$PLN_AREA_N
     
     dates <-  unique(processed_data$RIDE_START_DATE)
-    hours <- unique(processed_data$RIDE_START_HOUR)
     updateSelectInput(session,"date_1",choices = dates,selected = dates[1])
     updateSelectInput(session,"date_2",choices = dates,selected = dates[1])
-    updateSelectInput(session,"hour_1",choices = hours,selected = hours[1])
-    updateSelectInput(session,"hour_2",choices = hours,selected = hours[2])
-    
-    processed_data 
+    original_data <<- processed_data
+    original_data 
   })
   
   create_matrix <- reactive(function(process_ride_data){
@@ -94,13 +110,13 @@ shinyServer(function(input, output, session) {
     mat
   })
   
-  create_plot <- reactive(function(myflows,filename){
+  create_plot <- reactive(function(myflows,filename, k){
     
     diag(myflows) <- 0
     
     # Select flows that represent at least 20% of the sum of outgoing flows for 
     # each urban area.
-    flowSel1 <- firstflows(mat = myflows/rowSums(myflows)*100, method = "xfirst", k = 20)
+    flowSel1 <- firstflows(mat = myflows/rowSums(myflows)*100, method = "xfirst", k = k)
     
     # Select the dominant flows (incoming flows criterion)
     flowSel2 <- domflows(mat = myflows, w = colSums(myflows), k = 1)
@@ -123,58 +139,99 @@ shinyServer(function(input, output, session) {
     par(opar)
   })
   
-  #output$statst <- renderLeaflet({
-  #tmap_leaflet(qtm(sub_data))
-  #leaflet() %>%
-  #addProviderTiles(providers$OpenStreetMap) %>%
-  #addMarkers(ride_data, lat = ride_data$X.BOARDING, lng=ride_data$Y.BOARDING, clusterOptions = markerClusterOptions())
-  #})
   
-  output$ride_table <- renderDataTable({
-    get_data()(input$file1)
+  
+  #Do once
+  
+  observe({
+    cat("observed triggered \n")
+    inFile <- input$file11
+    if (!is.null(inFile)){
+      if  (inFile$name != data_file_name){
+        
+        processed_ride_data <- get_data()(input$file1)
+      }
+    }else{
+      processed_ride_data <- get_data()(NULL)
+      
+      if (first_load){
+        dates <-  unique(processed_data$RIDE_START_DATE)
+        updateSelectInput(session,"date_1",choices = dates,selected = dates[1])
+        updateSelectInput(session,"date_2",choices = dates,selected = dates[1])
+        first_load <<- FALSE
+      }
+    }
+    
+    if (!is.null(processed_ride_data)){
+      
+      date_1 <- input$date_1
+      date_2 <- input$date_2
+      
+      if (!is.null(date_1)){
+        
+        data <- subset(processed_ride_data,RIDE_START_DATE == date_1)
+        hours <-  unique(data$RIDE_START_HOUR)
+        hours <- sort(hours, decreasing = FALSE)
+        
+        updateSelectInput(session,"hour_1",choices = hours,selected = hours[1])
+      }
+      
+      if (!is.null(date_2)){
+        data <- subset(processed_ride_data,RIDE_START_DATE == date_2)
+        hours <-  unique(data$RIDE_START_HOUR)
+        hours <- sort(hours, decreasing = FALSE)
+        updateSelectInput(session,"hour_2",choices = hours,selected = hours[1])
+      }
+    }
+    
   })
   
   output$stat1 <- renderPlot({
-    cat("stat1 executed")
+    req(input$date_1,input$hour_1, cancelOutput = TRUE)
+    date_1 <- input$date_1
+    hour_1 <- input$hour_1
+    K_1 <- input$K_1
     
     processed_ride_data <- get_data()(input$file1)
     
-    hour_1 <- input$hour_1
-    date_1 <- input$date_1
-    subset_data_1 <- subset(processed_ride_data,c(RIDE_START_HOUR == hour_1 & RIDE_START_DATE == date_1))
+    subset_data_1 <- subset(processed_ride_data,RIDE_START_HOUR == hour_1 & RIDE_START_DATE == date_1)
     
     mat1 <- create_matrix()(subset_data_1)
     myflows1 <- prepflows(mat = mat1, i = "i", j = "j", fij = "fij")
     
-    
-    output$ride_table_1 <- renderDataTable(subset_data_1)
+    output$ride_table_1 <- renderDataTable(subset_data_1@data)
     output$flow_matrix_table_1 <- renderDataTable(mat1)
     
-    output$dominance_plot1 <- renderPlot({create_plot()(myflows1,hour_1)})
+    output$dominance_plot1 <- renderPlot({create_plot()(myflows1,hour_1,K_1)})
     statmat(mat = myflows1, output = "all", verbose = TRUE)
     
   })
   
   
   output$stat2 <- renderPlot({
-    cat("stat2 executed")
+    req(input$date_2,input$hour_2, cancelOutput = TRUE)
+    date_2 <- input$date_2
+    hour_2 <- input$hour_2
+    K_2 <- input$K_2
     
     processed_ride_data <- get_data()(input$file1)
     
-    hour_2 <- input$hour_2
-    date_2 <- input$date_2
-    subset_data_2 <- subset(processed_ride_data,c(RIDE_START_HOUR == hour_2 & RIDE_START_DATE == date_2))
+    
+    subset_data_2 <- subset(processed_ride_data,RIDE_START_HOUR == hour_2 & RIDE_START_DATE == date_2)
     
     mat2 <- create_matrix()(subset_data_2)
     myflows2 <- prepflows(mat = mat2, i = "i", j = "j", fij = "fij")
     
-    output$ride_table_2 <- renderDataTable(subset_data_2)
+    output$ride_table_2 <- renderDataTable(subset_data_2@data)
     output$flow_matrix_table_2 <- renderDataTable(mat2)
     
-    output$dominance_plot2 <- renderPlot({create_plot()(myflows2,hour_2)})
+    output$dominance_plot2 <- renderPlot({create_plot()(myflows2,hour_2,K_2)})
+    
     statmat(mat = myflows2, output = "all", verbose = TRUE)
     
   })
+  
+  
 })#end shinyserver
 
 
--- ui.r
+++ ui.r
@@ -4,32 +4,31 @@ library(leaflet)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Hello Shiny!"),
+  titlePanel("Bus Rider Flow Pattern"),
   
   navbarPage("Bus Rider Flow",
-             tabPanel("Interactive map",
+             tabPanel("Interactive Analysis",
                       sidebarLayout(
                         
                         # Sidebar with a slider input
                         sidebarPanel(width = 2,
                                      #dateInput(inputId ="date", label = "Date:", value = as.Date(c("2/17/16"),"%m/%d/%y"), format = "m/d/yy"),
                                      fileInput(inputId="file1", "Choose File"),
-                                     selectInput(inputId = "hour_1", label = "Hour 1", choices = NULL, multiple = FALSE, selectize = TRUE),
                                      selectInput(inputId = "date_1", label = "Date 1", choices = NULL, multiple = FALSE, selectize = TRUE),
+                                     selectInput(inputId = "hour_1", label = "Hour 1", choices = NULL, multiple = FALSE, selectize = TRUE),
+                                     sliderInput(inputId = "K_1", label ="K 1", min = 5, max = 100, value = 20, step = 5),
+                                     selectInput(inputId = "date_2", label = "Date 2", choices = NULL, multiple = FALSE, selectize = TRUE),
                                      selectInput(inputId = "hour_2", label = "Hour 2", choices = NULL, multiple = FALSE, selectize = TRUE),
-                                     selectInput(inputId = "date_2", label = "Date 2", choices = NULL, multiple = FALSE, selectize = TRUE)
-                                     
+                                     sliderInput(inputId = "K_2", label ="K 2", min = 5, max = 100, value = 20, step = 5)
                         ),
                         
                         # Show a plot of the generated distribution
                         mainPanel(
                           #leafletOutput("map", height= 600),
-                          plotOutput("stat1"),
-                          plotOutput("stat2"),
-                          plotOutput("dominance_plot1"),
-                          plotOutput("dominance_plot2"),
-                          textOutput("comstat", container = div, inline = FALSE)
-                          
+                          plotOutput("dominance_plot1",height= 600),
+                          plotOutput("dominance_plot2",height= 600),                   
+                          column(width = 6, h3("Statistic - 1"), plotOutput("stat1")),
+                          column(width = 6, h3("Statistic - 2"), plotOutput("stat2"))
                         )
                       )
              ), 
